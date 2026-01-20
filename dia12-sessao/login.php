4c9ce5ebb3eda9e929cad717218ab30204aee3fe
Kauanw10 <kauanw10@gmail.com>
2026-01-19 21:29:15 -0300

    Dia 13 - validação e tratamento de erros no backend

--- dia12-sessao/login.php
+++ dia12-sessao/login.php
@@ -1,12 +1,51 @@
 <?php 
-    $user = $_POST['usuario'];
-    $pass = $_POST['senha'];
+    $user = trim($_POST['usuario'] ?? '');
+    $pass = trim($_POST['senha'] ?? '');
+    
+    try {
+        validarUser($user);
+        validarPass($pass);
 
-    if ($user === "admin" && $pass = 1234) {
         session_start();
-        $_SESSION['logado'] = true;
-        require_once "dashboard.php";
-    }else {
-        header("Location: login.html");
+        $_SESSION['logado'] = (validarAdmin($user, $pass) ? true : false);
+        include_once "dashboard.php";
+
+    } catch (\Throwable $e) {
+        echo $e->getMessage();
+        include_once "login.html";
+    }
+
+    function validarUser($user) {
+        if (empty($user)) {
+            throw new Exception("Campo Usuário Vázio");
+            exit;
+        }
+
+        return [
+            'sucesso' => true
+        ];
+    }
+
+    function validarPass($pass){
+        if (empty($pass)) {
+            throw new Exception("Campo Senha vázio");
+            exit;
+        } elseif (strlen($pass) < 3) {
+           throw new Exception("Minimo 3 caracteres para a Senha.");
+           exit;
+        }
+
+        return [
+            'sucesso' => true
+        ];
+    }
+    
+    function validarAdmin($user, $pass) {
+        if (($user != "admin") || ($pass != "12345")) {
+            return false;
+        }
+
+       return true;
+
     }
 ?>
